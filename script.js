cd8931d0031e252b8df605ae04e6c772b3dcc1ed
Rovch <wesleyjacksonjr33@hotmail.com>
2018-02-10 20:01:09 -0600

    touched index.html, script.js, style,css

---
+++ script.js
@@ -0,0 +1,29 @@
+//------------------------- PseudoCode ---------------------------//
+// 1.  initiation
+// 2.  ask the player if it wants 1 or 2 players, then allow if player one enter a word- 
+//     if single player allow user to choose category and auto select a word from stored array
+// 3.  computer reads "word" then displays word length
+// 4.  computer displays life count and allows player 2 to begin to guess 
+// 5. player either guesses the word or loses the game due to not enough lives 
+// 6. reset.
+//------------------------ PseudoCode ---------------------------//
+
+
+var bank = " ";
+var wordArray = [];
+
+
+function myFunction() {
+
+  //Retrieves the "input" entered by user in the html form and logs it
+  bank = document.getElementById("inputContent").value;
+  console.log(bank)
+
+  //for loop will run until its equal to the statement aboves .length, it then pushes each character to an array. Then logs it
+  for (var i = 0; i < bank.length; i++) {
+    wordArray.push(bank.charAt(i));
+  }
+  console.log(wordArray);
+}
+
+//I dont want any of the remaining code for the game to run unless the top function has already ran?????????
